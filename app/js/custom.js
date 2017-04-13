bad9445c24b8edf1d370df81f0f6b88c444e6d05
baudeveloper <webmaster@baustein.ca>
2017-04-13 10:55:23 -0400

    Added upload images modal content.

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,15 +1,30 @@
-/*
-  Table of content:
-  1. Profile page
-  2. Miscellanous
-*/
+$(document).ready(function() {
 
-// 1. Profile page.
-// A. Show/Hide password on Profile page.
-$(".toggle_password input").click(function() {
-  if ($("#set_password").attr("type") == "password") {
-    $("#set_password").attr("type", "text");
-  } else {
-    $("#set_password").attr("type", "password");
-  }
+  /*
+    Table of content:
+    1. Profile page
+    2. Miscellanous
+  */
+
+  // 1. Profile page.
+  // A. Show/Hide password on Profile page.
+  $(".toggle_password input").click(function() {
+    if ($("#set_password").attr("type") == "password") {
+      $("#set_password").attr("type", "text");
+    } else {
+      $("#set_password").attr("type", "password");
+    }
+  });
+
+  // 2. Modal Windows
+  $('.select-date').datetimepicker({
+    defaultDate: "04/04/2017",
+    icons: {
+      time: 'fa fa-clock-o',
+      date: 'fa fa-calendar',
+      up: 'fa fa-arrow-up',
+      down: 'fa fa-arrow-down'
+    }
+  });
+
 });
