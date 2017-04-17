5d469a420edfe353f08f80c82f46d3582018690b
baudeveloper <webmaster@baustein.ca>
2017-04-17 08:37:53 -0400

    Added cropit.js bower package.

--- app/js/custom.js
+++ app/js/custom.js
@@ -150,7 +150,22 @@ $(document).ready(function() {
 
   // 7. Nav tabs to Select on Case page
   $('#media_type_select').on('change', function (e) {
-    $('.media_tab li a').eq($(this).val()).tab('show'); 
+    $('.media_tab li a').eq($(this).val()).tab('show');
+  });
+
+  // 8. Crop Image in Edit Media Modal window
+  $('#image-cropper').cropit();
+  // When user clicks select image button,
+  // open select file dialog programmatically
+  $('.select-image-btn').click(function() {
+    $('.cropit-image-input').click();
+  });
+  // Handle rotation
+  $('.rotate-cw-btn').click(function() {
+    $('#image-cropper').cropit('rotateCW');
+  });
+  $('.rotate-ccw-btn').click(function() {
+    $('#image-cropper').cropit('rotateCCW');
   });
 
 });
