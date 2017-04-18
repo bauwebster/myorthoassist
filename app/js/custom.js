b0bfaba1ac097e4ad25ad07f7161d4e310f38b62
baudeveloper <webmaster@baustein.ca>
2017-04-18 09:33:23 -0400

    Added solution for active tabs from different page.

--- app/js/custom.js
+++ app/js/custom.js
@@ -188,4 +188,16 @@ $(document).ready(function() {
     $(this).closest('.image-editor').cropit('rotateCCW');
   });
 
+  // 9. Hyperlink to active tab
+  // Javascript to enable link to tab
+  var hash = document.location.hash;
+  var prefix = "tab_";
+  if (hash) {
+    $('.nav-tabs a[href="'+hash.replace(prefix,"")+'"]').tab('show');
+  }
+  // Change hash for page-reload
+  $('.nav-tabs a').on('shown', function (e) {
+    window.location.hash = e.target.hash.replace("#", "#" + prefix);
+  });
+
 });
